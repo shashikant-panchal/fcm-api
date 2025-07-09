36859af17111c11f5117d9b5fbeea2647d0d36e2
Shashikant Panchal <123376887+shashikant-panchal@users.noreply.github.com>
2025-07-09 11:13:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-// const serviceAccount = require("./firebase-service-account.json");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
